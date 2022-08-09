1fbb12fa5c8f400f27e85fbfa85411d3926106fc
Vihaan Sharma <86086039+Lemirq@users.noreply.github.com>
2022-08-09 00:06:10 -0400

    Responsive Stats Preview Card Component with CSS Flexbox & CSS Grid

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const cards = [
 	{
+		name: 'Interactive and Responsive Card Details Form with CSS Flexbox',
+		difficulty: 'junior',
+		url: './Interactive Card Details Form/index.html',
+	},
+	{
 		name: 'Responsive OR Code Card Component using CSS Flexbox',
 		difficulty: 'newbie',
 		url: './QR Code Component/index.html',
@@ -25,15 +30,17 @@ const cards = [
 		url: './Order Summary Component/index.html',
 	},
 	{
-		name: 'Interactive and Responsive Card Details Form with CSS Flexbox',
-		difficulty: 'junior',
-		url: './Interactive Card Details Form/index.html',
+		name: 'Responsive Stats Preview Card Component with CSS Flexbox & CSS Grid',
+		difficulty: 'newbie',
+		url: './Stats Preview Card Component/index.html',
 	},
 ];
 
 for (let i = 0; i < cards.length; i++) {
 	const html = `
-	<div onclick="window.open('${cards[i].url}')" class="card ${cards[i].difficulty}">
+	<div style="animation-delay: ${i / 10}s;" onclick="window.open('${
+		cards[i].url
+	}')" class=" animate__animated animate__fadeIn ${cards[i].difficulty} card">
 	${cards[i].name}
 	</div>
 	`;
@@ -58,48 +65,76 @@ const lightness = '70%';
 
 newbie.addEventListener('click', () => {
 	newbie.style.setProperty('--lightness', lightness);
-	for (var i = 0; i < grid.length; i++) {
-		grid[i].style.display = 'none';
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].classList.add('animate__animated', 'animate__fadeOut');
 	}
-	for (let i = 0; i < newbieEl.length; i++) {
-		newbieEl[i].style.display = 'flex';
-	}
+	setTimeout(() => {
+		for (let i = 0; i < newbieEl.length; i++) {
+			document.querySelector('.card:not(.newbie)').style.display = 'none';
+			newbieEl[i].style.display = 'flex';
+			newbieEl[i].classList.remove('animate__fadeOut');
+			newbieEl[i].classList.add('animate__fadeIn');
+		}
+	}, 1100);
 });
 junior.addEventListener('click', () => {
 	junior.style.setProperty('--lightness', lightness);
-	for (var i = 0; i < grid.length; i++) {
-		grid[i].style.display = 'none';
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].classList.add('animate__animated', 'animate__fadeOut');
 	}
-	for (let i = 0; i < juniorEl.length; i++) {
-		juniorEl[i].style.display = 'flex';
-	}
+	setTimeout(() => {
+		for (let i = 0; i < juniorEl.length; i++) {
+			document.querySelector('.card:not(.junior)').style.display = 'none';
+			juniorEl[i].style.display = 'flex';
+			juniorEl[i].classList.remove('animate__fadeOut');
+			juniorEl[i].classList.add('animate__fadeIn');
+		}
+	}, 1100);
 });
+
 intermediate.addEventListener('click', () => {
 	intermediate.style.setProperty('--lightness', lightness);
-	for (var i = 0; i < grid.length; i++) {
-		grid[i].style.display = 'none';
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].classList.add('animate__animated', 'animate__fadeOut');
 	}
-	for (let i = 0; i < intermediateEl.length; i++) {
-		intermediateEl[i].style.display = 'flex';
-	}
+	setTimeout(() => {
+		for (let i = 0; i < intermediateEl.length; i++) {
+			document.querySelector('.card:not(.intermediate)').style.display =
+				'none';
+			intermediateEl[i].style.display = 'flex';
+			intermediateEl[i].classList.remove('animate__fadeOut');
+			intermediateEl[i].classList.add('animate__fadeIn');
+		}
+	}, 1100);
 });
 advanced.addEventListener('click', () => {
 	advanced.style.setProperty('--lightness', lightness);
-	for (var i = 0; i < grid.length; i++) {
-		grid[i].style.display = 'none';
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].classList.add('animate__animated', 'animate__fadeOut');
 	}
-	for (let i = 0; i < advancedEl.length; i++) {
-		advancedEl[i].style.display = 'flex';
-	}
+	setTimeout(() => {
+		for (let i = 0; i < advanced.length; i++) {
+			document.querySelector('.card:not(.advanced)').style.display =
+				'none';
+			advancedEl[i].style.display = 'flex';
+			advancedEl[i].classList.remove('animate__fadeOut');
+			advancedEl[i].classList.add('animate__fadeIn');
+		}
+	}, 1100);
 });
 guru.addEventListener('click', () => {
 	guru.style.setProperty('--lightness', lightness);
-	for (var i = 0; i < grid.length; i++) {
-		grid[i].style.display = 'none';
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].classList.add('animate__animated', 'animate__fadeOut');
 	}
-	for (let i = 0; i < guruEl.length; i++) {
-		guruEl[i].style.display = 'flex';
-	}
+	setTimeout(() => {
+		for (let i = 0; i < guruEl.length; i++) {
+			document.querySelector('.card:not(.guru)').style.display = 'none';
+			guruEl[i].style.display = 'flex';
+			guruEl[i].classList.remove('animate__fadeOut');
+			guruEl[i].classList.add('animate__fadeIn');
+		}
+	}, 1100);
 });
 
 clear.addEventListener('click', () => {
@@ -109,6 +144,12 @@ clear.addEventListener('click', () => {
 	advanced.style.setProperty('--lightness', '80%');
 	guru.style.setProperty('--lightness', '80%');
 	for (let i = 0; i < grid.length; i++) {
-		grid[i].style.display = '';
+		grid[i].classList.remove('animate__fadeIn');
+		grid[i].classList.add('animate__fadeOut');
+		setTimeout(() => {
+			grid[i].classList.remove('animate__fadeOut');
+			grid[i].style.display = 'flex';
+			grid[i].classList.add('animate__fadeIn');
+		}, 1100);
 	}
 });
